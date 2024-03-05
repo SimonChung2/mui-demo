00756c2493f7a3b655f2db74d9a968d01727b600
SimonChung2 <simonmchung@gmail.com>
2024-03-05 10:03:05 -0500

    init LearnMUI repo

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,19 @@
+import { Typography } from '@mui/material'
+
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant='h1'>h1 Heading</Typography>
+      <Typography variant='h2'>h1 Heading</Typography>
+      <Typography variant='h3'>h1 Heading</Typography>
+      <Typography variant='h4'>h1 Heading</Typography>
+      <Typography variant='h5'>h1 Heading</Typography>
+      <Typography variant='h6'>h1 Heading</Typography>
+      
+      <Typography variant='subtitle1'>Sub title 1</Typography>
+      <Typography variant='subtitle2'>Sub title 2</Typography>
+    </div>
+  )
+}
+
+export default MuiTypography
